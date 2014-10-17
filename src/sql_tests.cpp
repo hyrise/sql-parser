75a4bed720076b9bea9794a9c34ca034300d6f35
Pedro <pedroflemming@gmail.com>
2014-10-17 16:25:55 +0200

    added multithreading test

--- src/sql_tests.cpp
+++ src/sql_tests.cpp
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <string>
 #include <cassert>
+#include <thread>
 
 #define STREQUALS(str1, str2) std::string(str1).compare(std::string(str2)) == 0
 
@@ -70,12 +71,19 @@ void SelectTest2() {
 	printf("passed!\n");
 }
 
-void SelectTest3() {
-	printf("Test: SelectTest3... ");
+int parse_count = 0;
+void SelectTest3(bool print) {
+	if (print) printf("Test: SelectTest3... ");
 	fflush(stdout);
 
 	const char* sql = "SELECT name, AVG(age) FROM table GROUP BY name";
+	parse_count++;
+
 	Statement* stmt = SQLParser::parseSQL(sql);
+
+	if (parse_count != 1) printf("+");
+	parse_count--;
+
 	ASSERT(stmt != NULL);
 	ASSERT(stmt->_type == eSelect);
 
@@ -92,9 +100,27 @@ void SelectTest3() {
 	ASSERT(select->_group_by->size() == 1);
 	ASSERT_STR("name", select->_group_by->at(0)->name);
 
-	printf("passed!\n");
+	if (print) printf("passed!\n");
 }
 
+
+/** Multithread Test **/
+void multithreadTest(int numberOfRuns, int id) {
+	for (int n = 0; n < numberOfRuns; ++n) {
+		SelectTest3(false);
+	}
+}
+void ThreadSafeTest(uint numThreads, uint runsPerThread) {
+	printf("Starting multithread-test... ");
+	std::thread* threads = new std::thread[numThreads];
+	for (int n = 0; n < numThreads; ++n) {
+		threads[n] = std::thread(multithreadTest, runsPerThread, n);
+	}
+	for (int n = 0; n < numThreads; ++n) {
+		threads[n].join();
+	}
+	printf("finished!\n");
+}
 
 
 int main(int argc, char *argv[]) {
@@ -104,7 +130,8 @@ int main(int argc, char *argv[]) {
 
 	SelectTest1();
 	SelectTest2();
-	SelectTest3();
+	SelectTest3(true);
+	ThreadSafeTest(10, 1000);
 
 	printf("\n## Finished running all tests...\n");
 	printf("######################################\n");
