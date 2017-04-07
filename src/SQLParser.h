e16925e7a5f9d2e05592da374bc18c1d65ae04c9
Pedro Flemming <pedroflemming@gmail.com>
2017-04-07 16:16:25 +0200

    add parseSQLString method with output parameter instead of return value

--- src/SQLParser.h
+++ src/SQLParser.h
@@ -9,10 +9,20 @@ namespace hsql {
   // Static methods used to parse SQL strings.
   class SQLParser {
    public:
+    // Parses a given constant character SQL string into the result object.
+    static bool parseSQLString(const char* sql, SQLParserResult* result);
+
+    // Parses a given SQL string into the result object.
+    static bool parseSQLString(const std::string& sql, SQLParserResult* result);
+
     // Parses a given constant character SQL string.
+    // Note: This is kept for legacy reasons. It is recommended to use
+    //       the (const char*, SQLParserResult*) implementation.
     static SQLParserResult* parseSQLString(const char* sql);
 
     // Parses an SQL std::string.
+    // Note: This is kept for legacy reasons. It is recommended to use
+    //       the (const std::string&, SQLParserResult*) implementation.
     static SQLParserResult* parseSQLString(const std::string& sql);
 
    private:
--- test/sql_grammar_test.cpp
+++ test/sql_grammar_test.cpp
@@ -79,23 +79,22 @@ TEST(AutoGrammarTest) {
     start = std::chrono::system_clock::now();
 
     // Parsing
-    SQLParserResult* result = SQLParser::parseSQLString(sql.c_str());
+    SQLParserResult result;
+    SQLParser::parseSQLString(sql.c_str(), &result);
 
     end = std::chrono::system_clock::now();
     std::chrono::duration<double> elapsed_seconds = end - start;
     double us = elapsed_seconds.count() * 1000 * 1000;
 
-    if (expectFalse == result->isValid()) {
+    if (expectFalse == result.isValid()) {
       printf("\033[0;31m{  failed}\033[0m\n");
-      printf("\t\033[0;31m%s (L%d:%d)\n\033[0m", result->errorMsg(), result->errorLine(), result->errorColumn());
+      printf("\t\033[0;31m%s (L%d:%d)\n\033[0m", result.errorMsg(), result.errorLine(), result.errorColumn());
       printf("\t%s\n", sql.c_str());
       numFailed++;
     } else {
       // TODO: indicate whether expectFalse was set
       printf("\033[0;32m{      ok} (%.1fus)\033[0m %s\n", us, sql.c_str());
     }
-
-    delete result;
   }
 
   if (numFailed == 0) {
