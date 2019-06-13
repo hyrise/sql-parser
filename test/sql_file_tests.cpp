0321f82eb139d60cbc28e82004b92f2a2fa4ee89
Julian Menzler <julianmenzler@gmail.com>
2019-06-13 21:10:28 +0200

    Implement FileBasedQueryTest

--- test/sql_file_tests.cpp
+++ test/sql_file_tests.cpp
@@ -4,17 +4,17 @@
 #include "util/sqlhelper.h"
 #include "sql_asserts.h"
 
-#include <cstring>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <streambuf>
 #include <string>
+#include <vector>
 
 
 using namespace hsql;
-
 namespace fs = std::filesystem;
+
 
 std::string readFileContents(const std::string& file_path) {
   std::ifstream t(file_path.c_str());
@@ -23,33 +23,25 @@ std::string readFileContents(const std::string& file_path) {
   return text;
 }
 
-bool isValid(const std::string &query) {
-  // Assert single query only
-  return true;
-}
-
-bool testSqlQueryFile(const std::string &file_path) {
+bool runSqlQueryFromFile(const std::string &file_path) {
   std::string query = readFileContents(file_path);
 
-  if(!isValid(query)) mt::printFailed(std::string("invalid sql query: " + file_path).c_str());
-  else {
-    SQLParserResult result;
-    SQLParser::parse(query.c_str(), &result);
-    if (!result.isValid()) {
-      mt::printFailed(file_path.c_str());
-      printf("%s           %s (L%d:%d)%s\n", mt::red(), result.errorMsg(), result.errorLine(), result.errorColumn(), mt::def());
-    } else {
-      mt::printOk(file_path.c_str());
-      return true;
-    }
+  SQLParserResult result;
+  SQLParser::parse(query.c_str(), &result);
+  if (!result.isValid()) {
+    mt::printFailed(file_path.c_str());
+    printf("%s           %s (L%d:%d)%s\n", mt::red(), result.errorMsg(), result.errorLine(), result.errorColumn(), mt::def());
+  } else {
+    mt::printOk(file_path.c_str());
+    return true;
   }
   return false;
 }
 
-TEST(SQLFileGrammarTests) {
+TEST(FileBasedSQLGrammarTests) {
   std::string rootPath = "test/queries/";
 
-  // Create a sorted list of filepaths to .sql files
+  // Create sorted list of filepaths to .sql files
   std::vector<std::string> filePaths;
   for(auto& entry : fs::recursive_directory_iterator(rootPath)) {
     if (fs::is_regular_file(entry) && fs::path(entry).extension() == ".sql") {
@@ -58,7 +50,18 @@ TEST(SQLFileGrammarTests) {
   }
   sort(filePaths.begin(), filePaths.end());
 
-  std::cout << filePaths.size() << std::endl;
+  // Run queries
+  int queryRuns = 0;
+  int queryFails = 0;
+  for(auto& filePath : filePaths) {
+    bool success = runSqlQueryFromFile(filePath);
+    if(!success) queryFails++;
+    queryRuns++;
+  }
+
+  ASSERT_EQ(queryFails, 45)
+}
+
 TEST(QueryDetailTestTPCH20) {
   std::string query = readFileContents("test/queries/tpc-h-20.sql");
 
