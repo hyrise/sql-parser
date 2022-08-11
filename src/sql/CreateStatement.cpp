493c8873d3d3392f95fd243626fb804dfc654e8e
septicmk <septic.mk@gmail.com>
2022-08-11 09:59:32 +0800

    Move CreateStatement functions to `CreateStatement.cpp`

--- src/sql/CreateStatement.cpp
+++ src/sql/CreateStatement.cpp
@@ -1,7 +1,59 @@
 
+#include "SelectStatement.h"
 #include "CreateStatement.h"
 
 namespace hsql {
+
+// CreateStatemnet
+CreateStatement::CreateStatement(CreateType type)
+    : SQLStatement(kStmtCreate),
+      type(type),
+      ifNotExists(false),
+      filePath(nullptr),
+      schema(nullptr),
+      tableName(nullptr),
+      indexName(nullptr),
+      indexColumns(nullptr),
+      columns(nullptr),
+      tableConstraints(nullptr),
+      viewColumns(nullptr),
+      select(nullptr){};
+
+CreateStatement::~CreateStatement() {
+  free(filePath);
+  free(schema);
+  free(tableName);
+  free(indexName);
+  delete select;
+
+  if (columns != nullptr) {
+    for (ColumnDefinition* def : *columns) {
+      delete def;
+    }
+    delete columns;
+  }
+
+  if (tableConstraints != nullptr) {
+    for (TableConstraint* def : *tableConstraints) {
+      delete def;
+    }
+    delete tableConstraints;
+  }
+
+  if (indexColumns != nullptr) {
+    for (char* column : *indexColumns) {
+      free(column);
+    }
+    delete indexColumns;
+  }
+
+  if (viewColumns != nullptr) {
+    for (char* column : *viewColumns) {
+      free(column);
+    }
+    delete viewColumns;
+  }
+}
 
 void CreateStatement::setColumnDefsAndConstraints(std::vector<TableElement*>* tableElements) {
   columns = new std::vector<ColumnDefinition*>();
