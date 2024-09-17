d3b346948cad61b1f6449c4708086a87921763ac
dey4ss <27929897+dey4ss@users.noreply.github.com>
2024-09-17 14:37:40 +0200

    add comment

--- src/sql/ImportExportOptions.h
+++ src/sql/ImportExportOptions.h
@@ -4,7 +4,8 @@
 namespace hsql {
 
 // Name unchanged for compatibility. Historically, this was only used for import statements before we introduced export
-// statements (`COPY ... TO`). We did not change the enum name to accomodate forks.
+// statements (`COPY ... TO`). We did not change the enum name to accomodate forks. In the grammar, however, we call the
+// corresponding terminal symbol `file_type` and use it for both `ImportStatement` and `ExportStatement`.
 enum ImportType {
   kImportCSV,
   kImportTbl,  // Hyrise file format.
